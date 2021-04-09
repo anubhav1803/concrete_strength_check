e03f8b090b797865d22f4b7e5fa87bcd016e268b
anubhav1803 <anubhavchauhan1751@gmail.com>
2021-04-09 16:08:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/templates',methods=['GET'])
+@app.route('/templates/index.html',methods=['GET'])
 def Home():
     return render_template('index.html', template_folder='../templates')
 
